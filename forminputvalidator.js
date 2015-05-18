df43f857719efaa3c56d63345509fc6c568af747
omersagsoz <omersagsoz@omers-mbp>
2015-05-18 17:58:55 +0200

    18.05.2015 17:55 JS validation.

--- forminputvalidator.js
+++ forminputvalidator.js
@@ -1,102 +1,50 @@
 
 function chkBookAddFormular() {
-/*
-    letters = /^[A-Za-zÖöÄäÜü]+$/;
-    if (document.bookadd.vorname.value == "" || !document.bookadd.vorname.value.match(letters)
-	|| document.bookadd.vorname.value == "" || !document.bookadd.vorname.value.match(letters) 
-	|| document.bookadd.nachname.value == "" || !document.bookadd.nachname.value.match(letters)
-	|| document.bookadd.buchautor.value == "" || !document.bookadd.buchautor.value.match(letters)) {
+    var letters = /^[A-Za-zÖöÄäÜü]/;
+    if (!document.bookadd.vorname.value.match(letters)) {
         alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben!");
         document.bookadd.vorname.focus();
+        document.getElementByName("vorname")[0].style.borderColor = "red";
         return false;
     }
-    if (document.bookadd.nachname.value == "") {
-        alert("Bitte Nachname eingeben!");
+    if (!document.bookadd.nachname.value.match(letters)) {
+        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben!");
         document.bookadd.nachname.focus();
+        document.getElementByName("nachname")[0].style.borderColor = "red";
         return false;
     }
-    if (document.bookadd.titel.value == "") {
-        alert("Bitte Buchtitel eingeben!");
-        document.bookadd.titel.focus();
-        return false;
-    }
-    if (document.bookadd.autor.value == "") {
-        alert("Bitte Autor eingeben!");
+
+    if (!document.bookadd.autor.value.match(letters)) {
+       alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben!");
         document.bookadd.autor.focus();
-        return false;
-    }
-    if (document.bookadd.isbn.value == "") {
-        alert("Bitte ISBN eingeben!");
-        document.bookadd.isbn.focus();
-        return false;
-    }
-    if (document.bookadd.kapitel.value == "") {
-        alert("Bitte Kapitel eingeben!");
-        document.bookadd.kapitel.focus();
-        return false;
-    }
-    if (document.bookadd.jahr.value == "") {
-        alert("Bitte Jahr eingeben!");
-        document.bookadd.jahr.focus();
-        return false;
-    }
-    if (document.bookadd.auflage.value == "") {
-        alert("Bitte Auflage eingeben!");
-        document.bookadd.auflage.focus();
+        document.getElementByName("autor")[0].style.borderColor = "red";
         return false;
     }
     
-    
-}
-function chkBookAddFormular()
-  {
-    //Check Autor, Vorname, Name 
-    letters = /^[A-Za-zÖöÄäÜü]+$/;
-    if (document.bookadd.vorname.value == "" || !document.bookadd.vorname.value.match(letters)
-	|| document.bookadd.vorname.value == "" || !document.bookadd.vorname.value.match(letters) 
-	|| document.bookadd.nachname.value == "" || !document.bookadd.nachname.value.match(letters)
-	|| document.bookadd.buchautor.value == "" || !document.bookadd.buchautor.value.match(letters)) {
+    var isbn = /^[0-9]{13}/;
+    if (!document.bookadd.vorname.value.match(isbn)) {
         alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben!");
-        document.bookadd.vorname.focus();
-      document.bookadd.vorname.style.borderColor = "red";
-      return false;
+        document.bookadd.isbn.focus();
+        document.getElementByName("isbn")[0].style.borderColor = "red";
+        return false;
     }
-    if(!buchstaben.test(document.getElementsByName("vorname")[0].value)) {
-      alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
-      document.getElementsByName("vorname")[0].focus();
-      document.getElementsByName("vorname")[0].style.borderColor = "red";
-      return false;
-    }
-    if(!buchstaben.test(document.getElementsByName("name")[0].value)) {
-      alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
-      document.getElementsByName("name")[0].focus();
-      document.getElementsByName("name")[0].style.borderColor = "red";
-      return false;
-    }
-    //Ende: Überprüfung der Eingabefelder Autor, Vorname & Name
-*/
-    var isbn = /^[0-9]{13}$/;
-    if( !document.bookadd.vorname.value.match(isbn)){
-      alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
-      document.bookadd.isbn.focus();
-      document.bookadd.isbn.style.borderColor = "red";
-      return false;
-    }
-    var jahr = /^[0-9]{4}$/;
+
+    var jahr = /^[0-9]{4}/;
     var maxJahr = 2015;
-   if(document.bookadd.jahr.value <= maxJahr || document.bookadd.jahr.value.match(jahr)) {
-       alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
-       document.bookadd.jahr.focus();
-       document.bookadd.jahr.style.borderColor = "red";
-       return false;
+    if (document.bookadd.jahr.value <= maxJahr || !document.bookadd.jahr.value.match(jahr)) {
+        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben!");
+        document.bookadd.jahr.focus();
+         document.getElementByName("auflage")[0].style.borderColor = "red";
+        return false;
     }
+    
     var auflage = /^[0-9]/;
-     if(document.bookadd.jahr.value.match(auflage)) {
-      alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
-      document.getElementsByName("auflage")[0].focus();
-      document.bookadd.jahr.value.match.style.borderColor = "red";
-      return false;
+    if (!document.bookadd.vorname.value.match(auflage)) {
+        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+        document.bookadd.auflage.focus();
+         document.getElementByName("auflage")[0].style.borderColor = "red";
+        return false;
     }
-    // Überprüfung war erfolgreich
     return true;
   }
+  
